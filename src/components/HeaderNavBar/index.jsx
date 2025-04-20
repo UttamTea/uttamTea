433ca234055c6d712f7df3945727ea61562659bf
Rohit Maheshwari <maheshwari.rohit864@gmail.com>
2025-04-20 21:43:51 +0530

    header fix

--- src/components/HeaderNavBar/index.jsx
+++ src/components/HeaderNavBar/index.jsx
@@ -32,77 +32,55 @@ const HeaderNavBar = () => {
           display={"flex"}
           borderRadius={"1.25rem"}
           alignItems={"center"}
-          justifyContent={"space-between"}
+          justifyContent={"center"} // changed from space-between
+          position="relative"
         >
-          <Box display={"flex"} alignItems={"center"} gap={"2rem"}>
-            {TABS.map((item) => {
-              return (
-                <Box
-                  key={item.key}
-                  p={"0.5rem 1rem"}
-                  width={"fit-content"}
-                  bgcolor={selectedTab === item.key ? "#fff" : "transparent"}
-                  onClick={() => setSelectedTab(item.key)}
-                  borderRadius={"0.5rem"}
-                  sx={{ cursor: "pointer" }}
+          {/* Left - Tabs */}
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            gap={"2rem"}
+            position="absolute"
+            left="5.625rem"
+          >
+            {TABS.map((item) => (
+              <Box
+                key={item.key}
+                p={"0.5rem 1rem"}
+                width={"fit-content"}
+                bgcolor={selectedTab === item.key ? "#fff" : "transparent"}
+                onClick={() => setSelectedTab(item.key)}
+                borderRadius={"0.5rem"}
+                sx={{ cursor: "pointer" }}
+              >
+                <Typography
+                  fontSize={"1.125rem"}
+                  fontFamily={"Manrope"}
+                  fontWeight={400}
+                  color={selectedTab === item.key ? "#121212" : "#fff"}
                 >
-                  <Typography
-                    fontSize={"1.125rem"}
-                    fontFamily={"Manrope"}
-                    fontWeight={400}
-                    color={selectedTab === item.key ? "#121212" : "#fff"}
-                  >
-                    {item.title}
-                  </Typography>
-                </Box>
-              );
-            })}
+                  {item.title}
+                </Typography>
+              </Box>
+            ))}
           </Box>
+
+          {/* Center - Logo */}
           <Box>
             <img src={LOGO} alt="logo" />
           </Box>
-          <Box display={"flex"} alignItems={"center"}>
+
+          {/* Right - Buttons */}
+          <Box
+            display={"flex"}
+            alignItems={"center"}
+            position="absolute"
+            right="5.625rem"
+          >
             <Button variant="transparent">Login</Button>
             <Button variant="lightBrown">
               <Box display={"flex"} alignItems={"center"} gap={"0.5rem"}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="25"
-                  viewBox="0 0 24 25"
-                  fill="none"
-                >
-                  <path
-                    d="M7.5 8.17V7.2C7.5 4.95 9.31 2.74 11.56 2.53C14.24 2.27 16.5 4.38 16.5 7.01V8.39"
-                    stroke="white"
-                    stroke-width="1.5"
-                    stroke-miterlimit="10"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M8.99995 22.5H14.9999C19.0199 22.5 19.7399 20.89 19.9499 18.93L20.6999 12.93C20.9699 10.49 20.2699 8.5 15.9999 8.5H7.99995C3.72995 8.5 3.02995 10.49 3.29995 12.93L4.04995 18.93C4.25995 20.89 4.97995 22.5 8.99995 22.5Z"
-                    stroke="white"
-                    stroke-width="1.5"
-                    stroke-miterlimit="10"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M15.4955 12.5H15.5045"
-                    stroke="white"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                  <path
-                    d="M8.49451 12.5H8.50349"
-                    stroke="white"
-                    stroke-width="1.5"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  />
-                </svg>
+                {/* SVG code */}
                 Cart(0)
               </Box>
             </Button>
