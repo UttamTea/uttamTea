b110ed343cc791517bd445f236675f4a75b959d1
rohit0174 <maheshwari.rohit864@gmail.com>
2025-08-16 21:37:49 +0530

    removed review section

--- src/pages/ProductDetail/index.jsx
+++ src/pages/ProductDetail/index.jsx
@@ -54,7 +54,7 @@ const ProductDetailPage = () => {
       <HeaderNavBar />
       <ProductDetailSection details={data} />
       <MarqueeAndAboutSection />
-      <ReviewSection />
+      {/* <ReviewSection /> */}
       <OtherBenefitsSection />
       <Footer />
     </>
